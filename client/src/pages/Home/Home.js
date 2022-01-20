2163ac0cf7d8176506952918810243ab03fda4d1
youssef910 <youssef.sadek910@gmail.com>
2022-01-20 22:08:04 +0000

    add sec 2

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../../App.css";
 import HomeSec1 from "./HomeSec1";
+import HomeSec2scrollDown from './HomeSec2scrollDown';
 
 const Home = () => {
 	return (
 		<div>
-			<HomeSec1 />
+			<HomeSec1 style={{margin:"50px"}}/>
+			<HomeSec2scrollDown/>
 		</div>
 	);
 };
