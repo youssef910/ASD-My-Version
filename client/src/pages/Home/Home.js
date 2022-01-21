fe2eae4a0d5fe3b2d506172c9f9a49723511aed4
youssef910 <youssef.sadek910@gmail.com>
2022-01-21 15:55:11 +0000

    add home sec 3 to the main home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import "../../App.css";
 import HomeSec1 from "./HomeSec1";
-import HomeSec2scrollDown from './HomeSec2scrollDown';
+import HomeSec2scrollDown        from "./HomeSec2scrollDown";
+import HomeSec3 from "./HomeSec3";
 
 const Home = () => {
 	return (
 		<div>
-			<HomeSec1 style={{margin:"50px"}}/>
-			<HomeSec2scrollDown/>
+			<HomeSec1 style={{ margin: "50px" }} />
+			<HomeSec2scrollDown />
+			<HomeSec3 />
 		</div>
 	);
 };
