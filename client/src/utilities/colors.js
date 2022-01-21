378faf6071e771381a189fb1ba5cf48b9f1c8f36
youssef910 <youssef.sadek910@gmail.com>
2022-01-21 17:44:22 +0000

    fix bug in color baleBlue

--- client/src/utilities/colors.js
+++ client/src/utilities/colors.js
@@ -1,10 +1,9 @@
-
-  const colors = {
+const colors = {
 	blue: "#1E6DD0",
-	baleBlue: "#A4EDB",
+	baleBlue: "#A4E4DB",
 	black: "#0A2229",
 	lightBg: "#FFF8F3",
 	peag: "#F4DFD0",
 };
 
-export default colors
+export default colors;
