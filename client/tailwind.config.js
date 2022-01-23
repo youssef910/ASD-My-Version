189315bc6cc5174d6eaddf768d4e8cbf694b3f14
youssef910 <youssef.sadek910@gmail.com>
2022-01-23 18:05:00 +0000

    install tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			backgroundColor: {
+				cBlue: "#1E6DD0",
+				cBaleBlue: "#A4E4DB",
+				cBlack: "#0A2229",
+				cLightBg: "#FFF8F3",
+				cPeag: "#F4DFD0",
+			},
+		},
+	},
+	plugins: [],
+};
