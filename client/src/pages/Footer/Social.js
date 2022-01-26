6f1e30b69ead4a72bff13eb49e29c5786469f77a
youssef910 <youssef.sadek910@gmail.com>
2022-01-26 23:37:29 +0000

    make the icon pigger

--- client/src/pages/Footer/Social.js
+++ client/src/pages/Footer/Social.js
@@ -2,23 +2,24 @@ import React from "react";
 import { Text, Footer, Box, Anchor, Button } from "grommet";
 import { Instagram, FacebookOption, Twitter } from "grommet-icons";
 import colors from "../../utilities/colors";
-
+import LinkedAnchor from "./LinkedAnchor";
+import { Link } from "react-router-dom";
 const Media = () => (
 	<Box direction="row" pad={"left"} gap="small" justify="center">
 		<Button
 			hoverIndicator
 			href="https://www.facebook.com/"
-			icon={<FacebookOption size="small" color="white" />}
+			icon={<FacebookOption size="medium" color="white" />}
 		/>
 		<Button
 			hoverIndicator
 			href="https://twitter.com/"
-			icon={<Twitter size="small" color="white" />}
+			icon={<Twitter size="medium" color="white" />}
 		/>
 		<Button
 			hoverIndicator
 			href="https://www.instagram.com/"
-			icon={<Instagram size="small" color="white" />}
+			icon={<Instagram size="medium" color="white" />}
 		/>
 	</Box>
 );
@@ -26,18 +27,13 @@ const Media = () => (
 function Social() {
 	return (
 		<div>
-			<Footer background={colors.black} pad="small">
+			<Footer background={colors.black} pad="medium">
 				<Media />
-				<Box direction="row" gap="small" alignSelf="end">
-					<Text hoverIndicator textAlign="end" size="xsmall">
-						Terms & Conditions
-					</Text>
-					<Text hoverIndicator textAlign="end" size="xsmall">
-						Privacy Policy
-					</Text>
+				<Box direction="row" gap="medium" alignSelf="end">
+					<LinkedAnchor title="Terms & Conditions" link="#" />
+					<LinkedAnchor title="Privacy Policy" link="#" />
 				</Box>
 			</Footer>
-		
 		</div>
 	);
 }
