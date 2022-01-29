8899504ef3b252288323f86dc7916d67272ac4ad
youssef910 <youssef.sadek910@gmail.com>
2022-01-29 18:05:31 +0000

    install boot starab

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { grommet } from "grommet/themes";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
 	<Grommet theme={grommet} plain>
